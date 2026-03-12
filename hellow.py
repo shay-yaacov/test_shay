31f8a09eb349d4267612fc6bd5b0113075c681a6
shay <shayyaa@gmail.com>
2026-03-12 13:40:05 +0200

    add function

--- hellow.py
+++ hellow.py
@@ -1,6 +1,16 @@
 # print statement
 #"prin statement2"
+
+def add_numbers(a, b):
+    return a + b
+
 print ("hello my friend")
 print ("aaa")
 print("123")
 print (" i am on branch")
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+result= add_numbers(num1, num2)
+print("The sum is:", result)    
+
+
