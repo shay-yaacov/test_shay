42b063c7d61a6b8badb47b2f66f3998d3950edaf
shay-yaacov <shayyaa@gmail.com>
2026-06-10 15:32:13 +0300

    qq

--- hellow.py
+++ hellow.py
@@ -5,7 +5,7 @@ def add_numbers(a, b):
     return a + b
 
 print ("hello my friend")
-print ("aaa")
+print ("aasssadfa")
 print("12346qaaaq")
 print (" i am on branch")
 num1 = int(input("Enter first number: "))
