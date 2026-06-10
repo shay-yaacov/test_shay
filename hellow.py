7fd4f3e74df036fdf59cd8bc7c3d5ce5dfb60980
shay-yaacov <shayyaa@gmail.com>
2026-06-10 12:35:22 +0300

    add env

--- hellow.py
+++ hellow.py
@@ -12,7 +12,7 @@ num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 result= add_numbers(num1, num2)
 print("The sum is:", result)    
-print ("i am on branch3")
+print ("i am on branch366")
 
 
 
